3a3222f7954d365fef9126e4560c374a397e8f97
Federico Bricker <fbricker@gmail.com>
2015-06-06 13:00:34 -0300

    Check errors during build on buildScript

--- buildPackage.sh
+++ buildPackage.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 dir=`dirname "$0"`
 cd "$dir"
-lime rebuild . ios
-lime rebuild . mac
-lime rebuild . android
-rm -f openfl-harfbuzz.zip
+
+lime rebuild . ios && \
+lime rebuild . mac && \
+lime rebuild . android && \
+rm -f openfl-harfbuzz.zip && \
 zip -r openfl-harfbuzz.zip extension haxelib.json include.xml project ndll dependencies -x "project/obj/*"

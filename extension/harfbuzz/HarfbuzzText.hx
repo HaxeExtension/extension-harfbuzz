b2239e4f5d24992de349acd8ec1fd6ab255ae49b
Federico Bricker <fbricker@gmail.com>
2016-10-04 15:56:10 -0300

    Small fixes

--- extension/harfbuzz/HarfbuzzText.hx
+++ extension/harfbuzz/HarfbuzzText.hx
@@ -4,14 +4,17 @@ import openfl.display.Sprite;
 
 class HarfbuzzText extends Sprite{
 
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
+
 	private static var cachedRenderer:OpenflHarfbuzzRenderer = null;
 	private static var shouldCache:Bool = false;
 	private static var cacheText:String = "XQ.";
-
-	public var quality:Float;
 	public static inline var LEFT:String = 'left';
 	public static inline var RIGHT:String = 'right';
 	public static inline var CENTER:String = 'center';
+
+	///////////////////////////////////////////////////////////////////////////
 	
 	public static function enableCache(text:String){
 		cacheText = text+" \r\t\n";
@@ -19,30 +22,27 @@ class HarfbuzzText extends Sprite{
 		cachedRenderer = null;
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
 	public static function disableCache(){
 		shouldCache = false;
 		cacheText = "XQ.";
 		cachedRenderer = null;
 	}
 
-	private function createText(text:String, width:Float, font:String, color:Int, size:Int, dictionary:String):HarfbuzzSprite{
-		var renderer:OpenflHarfbuzzRenderer = cachedRenderer;
-		if(renderer == null) renderer = new OpenflHarfbuzzRenderer(font, Math.round(size*quality), color, cacheText+text);
-		if(shouldCache) cachedRenderer = renderer;
-		
-		var spr:HarfbuzzSprite = renderer.renderText( text, width );
-		spr.scaleX = spr.scaleY = 1/quality;
-		return spr;
-	}
+	///////////////////////////////////////////////////////////////////////////
+	///////////////////////////////////////////////////////////////////////////
 
 	private var boxWidth:Float;
 	private var renderedText:HarfbuzzSprite;
-
+	public var quality:Float;
 	public var color:Int;
 	public var align:String;
 	public var text(default,set):String = '';
 	public var size(default,set):Int;
 	public var font:String;
+
+	///////////////////////////////////////////////////////////////////////////
 
 	public function new(font:String, size:Float=12, color:Int=0x000000, align=LEFT, width:Float=400, quality:Float = 1){
 		super();
@@ -55,6 +55,8 @@ class HarfbuzzText extends Sprite{
 		renderedText = null;
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
 	private function realign(){
 		if (renderedText == null) return;
 		if (align == CENTER) renderedText.x = Math.round((boxWidth-renderedText.textWidth/quality)/2-renderedText.minX/quality);
@@ -62,23 +64,33 @@ class HarfbuzzText extends Sprite{
 		if (align == RIGHT)  renderedText.x = Math.round(boxWidth-renderedText.textWidth/quality);
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
+	public function getTextWidth():Float {
+		if(renderedText == null) return 0;
+		return renderedText.textWidth/quality;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+
 	override private function set_width(width:Float):Float{
 		this.boxWidth = width;
 		realign();
 		return width;
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
 	override private function get_width() return boxWidth;
+
+	///////////////////////////////////////////////////////////////////////////
 
 	override private function get_height():Float {
 	 	if(renderedText == null) return 0;
 	 	return renderedText.boxHeight/quality;
 	}
 
-	public function getTextWidth():Float {
-		if(renderedText == null) return 0;
-		return renderedText.textWidth/quality;
-	}
+	///////////////////////////////////////////////////////////////////////////
 
 	private function set_size(size:Int):Int {
 		if(size == this.size) return size;
@@ -87,12 +99,29 @@ class HarfbuzzText extends Sprite{
 		return size;
 	}
 
+	///////////////////////////////////////////////////////////////////////////
+
+	private function createText(dictionary:String):HarfbuzzSprite{
+		var renderer:OpenflHarfbuzzRenderer = cachedRenderer;
+		if(renderer == null) renderer = new OpenflHarfbuzzRenderer(font, Math.round(size*quality), color, cacheText+text);
+		if(shouldCache) cachedRenderer = renderer;
+		
+		var spr:HarfbuzzSprite = renderer.renderText( text, boxWidth*quality );
+		spr.scaleX = spr.scaleY = 1/quality;
+		return spr;
+	}
+
+	///////////////////////////////////////////////////////////////////////////
+
 	private function set_text(text:String):String {
 		this.text=text;
 		if(renderedText != null) this.removeChild(renderedText);
-		renderedText = createText(text,boxWidth,font,color,size,text);
+		renderedText = createText(text);
 		this.addChild(renderedText);
 		realign();
 		return text;
 	}
+
+	///////////////////////////////////////////////////////////////////////////
+
 }
